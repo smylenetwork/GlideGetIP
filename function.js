19e1bd6a9023290328d8810afa5a11e4be3f03dc
smylenetwork <156674787+smylenetwork@users.noreply.github.com>
2024-01-15 16:21:05 +0200

    Update function.js

--- function.js
+++ function.js
@@ -2,7 +2,6 @@ window.function = async function () {
   // Use fetch API to get IP address
   const response = await fetch('https://api.ipify.org');
   const data = await response.json();
-
   // Return the IP address
   return data.ip;
 }
